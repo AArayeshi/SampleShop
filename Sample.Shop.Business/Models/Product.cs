f6b5dc812f69d6cb404dbc9607279d5cedf2c600
Amir <amir.arayeshi@yahoo.com>
2021-12-19 19:06:55 +1030

    Add project files.

---
+++ Sample.Shop.Business/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample.Shop.Business.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Sample.Shop.Data/ShopDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sample.Shop.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample.Shop.Data
+{
+    public class ShopDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ShopDbContext(DbContextOptions<ShopDbContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
