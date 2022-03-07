6e8d28e2188f139fdebe3da78357677c57225380
Mehran <mehran.ershadi@yahoo.com>
2022-03-07 10:43:46 +0330

    Adding quantity field to Model and making migration

--- Sample.Shop.Business/Models/CartProduct.cs
+++ Sample.Shop.Business/Models/CartProduct.cs
@@ -9,7 +9,7 @@ namespace Sample.Shop.Business.Models
         public int Id { get; set; }
         public int CartId { get; set; }
         public int ProductId { get; set; }
-
+        public int quantity { get; set; }
         public Cart Cart { get; set; }
         public Product Product { get; set; }
     }
